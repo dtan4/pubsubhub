003382105599e017414cef8e15d30c2be0412ca3
Daisuke Fujita <dtanshi45@gmail.com>
2019-04-18 19:44:40 +0900

    List all subscriptions

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/dtan4/pubsubhub
 
 go 1.12
+
+require (
+	cloud.google.com/go v0.37.4
+	google.golang.org/api v0.3.1
+)
--- main.go
+++ main.go
@@ -1,9 +1,61 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+
+	"cloud.google.com/go/pubsub"
+	"google.golang.org/api/iterator"
 )
 
+const (
+	usage = "pubsubhub <GCP_PROJECT_ID>"
+)
+
+const (
+	exitOK int = iota
+	exitError
+)
+
+func run(args []string) int {
+	if len(args) != 2 {
+		fmt.Fprintln(os.Stderr, usage)
+		return exitError
+	}
+	projectID := args[1]
+
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return exitError
+	}
+
+	ss := []*pubsub.Subscription{}
+	iter := client.Subscriptions(ctx)
+
+	for {
+		s, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return exitError
+		}
+
+		ss = append(ss, s)
+	}
+
+	for _, s := range ss {
+		fmt.Println(s.ID())
+	}
+
+	return exitOK
+}
+
 func main() {
-	fmt.Println("pubsubhub")
+	run(os.Args)
 }
