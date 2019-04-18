5cce7c22c4df8cbfaeb838f8d4b14c74cc438da3
Daisuke Fujita <dtanshi45@gmail.com>
2019-04-18 20:32:01 +0900

    Create pubSubClient to isolate logic

--- main.go
+++ main.go
@@ -20,9 +20,49 @@ const (
 	exitError
 )
 
+type pubSubClient struct {
+	client *pubsub.Client
+}
+
+func NewPubSubClient(client *pubsub.Client) *pubSubClient {
+	return &pubSubClient{
+		client: client,
+	}
+}
+
+func (c *pubSubClient) ListSubscriptions(ctx context.Context) ([]*pubsub.Subscription, error) {
+	ss := []*pubsub.Subscription{}
+	iter := c.client.Subscriptions(ctx)
+
+	for {
+		s, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+
+		if err != nil {
+			return []*pubsub.Subscription{}, errors.Wrap(err, "cannot iterate subscriptions")
+		}
+
+		c, err := s.Config(ctx)
+		if err != nil {
+			return []*pubsub.Subscription{}, errors.Wrapf(err, "cannot get subscription config of %s", s.ID())
+		}
+
+		// skip push subscription
+		if c.PushConfig.Endpoint != "" {
+			continue
+		}
+
+		ss = append(ss, s)
+	}
+
+	return ss, nil
+}
+
 func subscribe(ctx context.Context, s *pubsub.Subscription) error {
 	err := s.Receive(ctx, func(ctx context.Context, m *pubsub.Message) {
-		log.Printf("[subscription:%s message:%s] %s", s.ID(), m.ID, string(m.Data))
+		log.Printf("[subscription:%s message:%s] body:%q", s.ID(), m.ID, string(m.Data))
 		m.Ack()
 		log.Printf("[subscription:%s message:%s] ACK", s.ID(), m.ID)
 	})
@@ -46,33 +86,12 @@ func run(args []string) int {
 		log.Println(err)
 		return exitError
 	}
+	ps := NewPubSubClient(client)
 
-	ss := []*pubsub.Subscription{}
-	iter := client.Subscriptions(ctx)
-
-	for {
-		s, err := iter.Next()
-		if err == iterator.Done {
-			break
-		}
-
-		if err != nil {
-			log.Println(err)
-			return exitError
-		}
-
-		c, err := s.Config(ctx)
-		if err != nil {
-			log.Println(err)
-			return exitError
-		}
-
-		// skip push subscription
-		if c.PushConfig.Endpoint != "" {
-			continue
-		}
-
-		ss = append(ss, s)
+	ss, err := ps.ListSubscriptions(ctx)
+	if err != nil {
+		log.Println(err)
+		return exitError
 	}
 
 	g, ctx := errgroup.WithContext(ctx)
