907448f318e085a2b353cdb72771601f344f1a81
Daisuke Fujita <dtanshi45@gmail.com>
2019-04-23 17:12:25 +0900

    Create message publisher

--- Makefile
+++ Makefile
@@ -4,3 +4,6 @@ SRCS := $(shell find . -type f -name '*.go')
 
 bin/$(NAME): $(SRCS)
 	GO111MODULE=on go build -o bin/$(NAME) github.com/dtan4/pubsubhub/cmd/pubsubhub
+
+bin/$(NAME)-publisher: $(SRCS)
+	GO111MODULE=on go build -o bin/$(NAME)-publisher github.com/dtan4/pubsubhub/cmd/pubsubhub-publisher
---
+++ cmd/pubsubhub-publisher/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/pkg/errors"
+)
+
+const (
+	usage = "pubsubhub-publisher <GCP_PROJECT_ID> <PUBSUB_TOPIC> <INTERVAL_MILLISECOND>"
+)
+
+const (
+	defaultSeed = 123456789
+)
+
+const (
+	exitOK int = iota
+	exitError
+)
+
+type pubSubClient struct {
+	client *pubsub.Client
+}
+
+func NewPubSubClient(client *pubsub.Client) *pubSubClient {
+	return &pubSubClient{
+		client: client,
+	}
+}
+
+func (c *pubSubClient) PublishContinuously(ctx context.Context, topic string, interval time.Duration, done <-chan int) error {
+	t := c.client.Topic(topic)
+	if t == nil {
+		return errors.Errorf("topic %q not found", topic)
+	}
+
+	r := rand.New(rand.NewSource(defaultSeed))
+
+	i := 0
+
+	for {
+		select {
+		case <-done:
+			return nil
+		default:
+		}
+
+		body := fmt.Sprintf("%d - %d", i, r.Int())
+
+		res := t.Publish(ctx, &pubsub.Message{
+			Data: []byte(body),
+		})
+
+		id, err := res.Get(ctx)
+		if err != nil {
+			return errors.Wrap(err, "cannot publish message")
+		}
+
+		log.Printf("published id:%s body:%q", id, body)
+
+		time.Sleep(interval)
+		i++
+	}
+}
+
+func run(args []string) int {
+	if len(args) != 4 {
+		log.Println(usage)
+		return exitError
+	}
+	projectID, topic := args[1], args[2]
+
+	i, err := strconv.Atoi(args[3])
+	if err != nil {
+		log.Println(err)
+		return exitError
+	}
+	interval := time.Duration(i) * time.Millisecond
+
+	ctx := context.Background()
+
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		log.Println(err)
+		return exitError
+	}
+	ps := NewPubSubClient(client)
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+
+	done := make(chan int)
+
+	go func() {
+		<-sigCh
+		log.Println("Terminating...")
+		close(done)
+	}()
+
+	if err := ps.PublishContinuously(ctx, topic, interval, done); err != nil {
+		log.Println(err)
+		return exitError
+	}
+
+	return exitOK
+}
+
+func main() {
+	run(os.Args)
+}
